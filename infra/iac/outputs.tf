e25b7239a3a22250133abcd2997cb9c8439ea5f7
IcaroCaetano <icaro.caetano@gmail.com>
2025-08-13 18:55:12 -0300

    feat: add doc

--- infra/iac/outputs.tf
+++ infra/iac/outputs.tf
@@ -1,19 +1,26 @@
+##Exibe o nome do cluster Kind criado.
+#Útil para identificar o cluster ativo.
 output "cluster_name" {
   description = "Name of the Kind cluster"
   value       = kind_cluster.default.name
 }
-
+##Informa o caminho para o kubeconfig, necessário para interagir com o cluster.
+#Marcado como sensitive = true, ou seja, não será mostrado no log por segurança
 output "kubeconfig_path" {
   description = "Path to the kubeconfig file"
   value       = kind_cluster.default.kubeconfig_path
   sensitive   = true
 }
 
+##Fornece o comando para acessar a interface do Grafana localmente:
+#Abre o Grafana em http://localhost:3000.
 output "grafana_port_forward" {
   description = "Command to port-forward Grafana"
   value       = "kubectl port-forward -n infra svc/grafana 3000:80"
 }
 
+##Fornece o comando para acessar o Jenkins localmente:
+#Abre o Jenkins em http://localhost:8080.
 output "jenkins_port_forward" {
   description = "Command to port-forward Jenkins"
   value       = "kubectl port-forward -n infra svc/jenkins 8080:8080"
--- infra/iac/providers.tf
+++ infra/iac/providers.tf
@@ -1,9 +1,22 @@
+##Permite criar e gerenciar clusters Kind (Kubernetes IN Docker).
+#É usado no recurso kind_cluster para subir um cluster local de testes.
+#Não possui parâmetros obrigatórios, pois as configurações vêm do próprio recurso.
 provider "kind" {}
 
+##Conecta o OpenTofu diretamente ao cluster Kubernetes criado.
+#Usa o kubeconfig_path gerado pelo recurso kind_cluster para autenticação.
+#Permite criar recursos como:
+#Namespaces
+#Deployments
+#Services
+#ConfigMaps, etc.
 provider "kubernetes" {
   config_path = kind_cluster.default.kubeconfig_path
 }
 
+##Permite instalar e gerenciar pacotes Helm dentro do cluster.
+#O config_path aponta para o mesmo kubeconfig, garantindo que o Helm se conecte ao cluster recém-criado.
+#Usado para instalar aplicações como Grafana, Prometheus, Jenkins.
 provider "helm" {
   kubernetes {
     config_path = kind_cluster.default.kubeconfig_path
