7534b2c4cdaab80105470982182dd1254987a9c8
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-08-17 13:58:52 -0300

    Update secrets.tf

--- infra/iac/secrets.tf
+++ infra/iac/secrets.tf
@@ -1,7 +1,7 @@
-# Este arquivo define Secrets no Kubernetes usando Terraform, que armazenam informações sensíveis, como credenciais, de forma segura.
+# Este arquivo define Secrets no Kubernetes usando opentofu, que armazenam informações sensíveis, como credenciais, de forma segura.
 # Secret que armazena credenciais para acessar o GitHub Container Registry (GHCR) a partir do Jenkins
 
-# This file defines Secrets in Kubernetes using Terraform, which store sensitive information, such as credentials, securely.
+# This file defines Secrets in Kubernetes using opentofu, which store sensitive information, such as credentials, securely.
 Secret that stores credentials to access GitHub Container Registry (GHCR) from Jenkins
 resource "kubernetes_secret" "ghcr" {
   metadata {
@@ -12,8 +12,8 @@ resource "kubernetes_secret" "ghcr" {
     # Namespace where the Secret will be created, using the previously defined infra namespace.
     namespace = kubernetes_namespace.infra.metadata[0].name
   }
-  # Valores sensíveis armazenados no Secret (codificados em base64 pelo Terraform):
-  # Sensitive values stored in the Secret (base64 encoded by Terraform):
+  # Valores sensíveis armazenados no Secret (codificados em base64 pelo opentofu):
+  # Sensitive values stored in the Secret (base64 encoded by opentofu):
   data = {
     # Nome do usuário do GHCR (variável var.ghcr_username).
     # GHCR username (variable var.ghcr_username).
