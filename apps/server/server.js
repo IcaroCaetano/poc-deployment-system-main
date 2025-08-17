56726dd4801e9f84b71a5ddaf2f61d6dadd37dc8
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-08-17 06:52:16 -0300

    Update server.js

--- apps/server/server.js
+++ apps/server/server.js
@@ -1,11 +1,15 @@
 // Importa a aplicação Express definida no arquivo app.js
+// Import the Express application defined in the app.js file
 const app = require("./app");
 
 // Define a porta do servidor
-// Usa a variável de ambiente PORT, se existir, caso contrário, usa 3000
-const port = process.env.PORT || 3000;
+// Usa a variável de ambiente PORT, se existir, caso contrário, usa 3001
+// Set the server port
+// Uses the PORT environment variable, if it exists, otherwise uses 3001
+const port = process.env.PORT || 3001;
 
 // Inicia o servidor e escuta na porta definida
+// Start the server and listen on the defined port
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
